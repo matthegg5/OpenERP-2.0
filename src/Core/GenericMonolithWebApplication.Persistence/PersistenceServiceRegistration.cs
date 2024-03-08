32514f0af7c3bd0a4bc0134090850e429da6ffff
matthegg5 <matthegg5@users.noreply.github.com>
2024-03-08 22:06:24 +0000

    added AspNetCore Identity in own database (#17)

--- src/Core/GenericMonolithWebApplication.Persistence/PersistenceServiceRegistration.cs
+++ src/Core/GenericMonolithWebApplication.Persistence/PersistenceServiceRegistration.cs
@@ -5,6 +5,8 @@ using GenericMonolithWebApplication.Infrastructure.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace GenericMonolithWebApplication.Infrastructure.Persistence
 {
@@ -13,6 +15,11 @@ namespace GenericMonolithWebApplication.Infrastructure.Persistence
         public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<GenericDbContext>(options => options.UseMySQL(configuration.GetConnectionString("GenericDbConnectionString")));
+            services.AddDbContext<IdentityDbContext>(options => options.UseMySQL(configuration.GetConnectionString("IdentityDbConnectionString")));
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                    .AddEntityFrameworkStores<IdentityDbContext>();
+
             services.AddScoped(typeof(IAsyncRepository<>), typeof(BaseRepository<>));
             services.AddScoped<IPartRepository, PartRepository>();
 
